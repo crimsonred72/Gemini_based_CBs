32308fb4a306c0bf969e08ae9b6e11e24c370d95
Abhinav <168896686+crimsonred72@users.noreply.github.com>
2025-01-02 21:34:07 +0530

    Update Main.py

--- Wise_Beggar/Main.py
+++ Wise_Beggar/Main.py
@@ -1,5 +1,5 @@
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyBsCw7_w-E_RBq-hF2mI8p9t51fHbp8xdw")
+genai.configure(api_key="Enter your Gemini API KEY")
 model = genai.GenerativeModel(model_name="gemini-1.5-flash")
 
 prompt = "Pretend as a homeless beggar chatbot, he is a tough person, he is old and wise, he can give great advice to people who ask, he speaks very less but true, and his language is harsh, but he never speaks explicitly"
